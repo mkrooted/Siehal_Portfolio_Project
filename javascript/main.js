65ff4d26623a9057235f70e38d1a0f3cd227054f
mkrooted <koreshov.m@gmail.com>
2016-07-14 14:41:25 +0300

    Cleared and reformated code a bit. Fixed hover view of arrows. Misc additions.

--- javascript/main.js
+++ javascript/main.js
@@ -2,27 +2,13 @@
  * Created by mkrooted on 11.07.2016.
  */
 var currentSector = 0;
-
-function prepareSectors(offset){
-    for(var i=1;i<5;i++){
-        var degrees = 90 * (i - 1) + 90*offset;
-        $("#sector"+i).css({"transform": "rotate("+degrees+"deg)"});
-    }
-}
 function rotateRight(){
     $(".circle-content").css({"transform": "rotate("+ ++currentSector*90 +"deg)"});
-    console.info("CurrentSector = ", currentSector);
 }
 function rotateLeft(){
     $(".circle-content").css({"transform": "rotate("+ --currentSector*90 +"deg)"});
-    console.info("CurrentSector = ", currentSector);
 }
-
 $(function(){
-    $("#right").click(function () {
-        rotateLeft();
-    });
-    $("#left").click(function () {
-        rotateRight();
-    });
+    $("#right").click(rotateLeft);
+    $("#left").click(rotateRight);
 });
